752b1e533e958efef765b0ff812a5436964906b5
Vadim Suboch <ueberaccelarate@gmail.com>
2022-08-03 13:45:39 +0300

    metis: Failed to download from mirror set [fixed]

--- ports/metis/portfile.cmake
+++ ports/metis/portfile.cmake
@@ -4,7 +4,7 @@ set(OPTIONS -DSHARED=OFF)
 set(METIS_VERSION 5.1.0)
 
 vcpkg_download_distfile(ARCHIVE
-    URLS "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-${METIS_VERSION}.tar.gz"
+    URLS "https://src.fedoraproject.org/lookaside/pkgs/metis/metis-${METIS_VERSION}.tar.gz/sha512/deea47749d13bd06fbeaf98a53c6c0b61603ddc17a43dae81d72c8015576f6495fd83c11b0ef68d024879ed5415c14ebdbd87ce49c181bdac680573bea8bdb25/metis-${METIS_VERSION}.tar.gz"
     FILENAME "metis-${METIS_VERSION}.tar.gz"
     SHA512 deea47749d13bd06fbeaf98a53c6c0b61603ddc17a43dae81d72c8015576f6495fd83c11b0ef68d024879ed5415c14ebdbd87ce49c181bdac680573bea8bdb25
 )
